81b732fc9610e2a9b51c933f396e15d760e62433
molk95 <molk.saouabi@gmail.com>
2020-06-30 17:10:38 +0200

    eslintrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,20 @@
-module.exports={
-    entry:[
-        './src/index.js'
-    ], 
-    output: {
-        path: __dirname + '/dist',
-        publicPath:'/',
-        filename: 'bundle.js'
-    
-},
-devServer: {
-    contentBase: './dist'
-},
-module: {
-
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
     rules: [
-        {test: '/\.(.js|.jsx)$/',
-        exclude: /node_module/,
-        use:['babel-loader']}
-    ]
-},
-resolve:{
-    extensions:['js','jsx']
-}
-}
+      { test: "/.(.js|.jsx)$/", exclude: /node_module/, use: ["babel-loader"] },
+      { test: "/.(.js|.jsx)$/", exclude: /node_module/, use: ["eslint-loader"] },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
